38d67089d01178e1f3da3124654ed57c3b9755b9
JaeWook Lee <2jae66@gmail.com>
2021-01-08 08:58:45 +0900

    generate

--- Mastering-RxSwift-master/main/Subjects/AsyncSubject.playground/Contents.swift
+++ Mastering-RxSwift-master/main/Subjects/AsyncSubject.playground/Contents.swift
@@ -34,3 +34,14 @@ enum MyError: Error {
    case error
 }
 
+let subject = AsyncSubject<Int>()
+
+subject.subscribe{print($0)}
+    .disposed(by: bag)
+
+subject.onNext(1)
+subject.onNext(2)
+subject.onNext(3)
+
+//subject.onCompleted()w
+subject.onError(MyError.error)
